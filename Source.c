a5a5254582af408424100166c7f2811f460d0237
arshbrar6789 <39374092+arshbrar6789@users.noreply.github.com>
2021-09-27 21:04:55 -0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() 
+{
+	puts("This is my first program on C language");   /*Puts function can insert String */
+	puts("I am on third chapter of this book and learning I/O"); // This is second type to insert commets in the program "//" 
+	return 3;
+}
